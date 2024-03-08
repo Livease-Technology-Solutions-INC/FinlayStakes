d909a2beff42495449f2f248aec0222a6078b97d
Ajita9496 <asthanaajita9496@gmail.com>
2024-03-08 21:32:02 +0530

    shedule call page added

--- src/components/Pagetitle.jsx
+++ src/components/Pagetitle.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Box, Container, Breadcrumbs, Link, Typography } from '@mui/material';
 
-function PageTitle({ title, companyName = 'liviileads' }) {
+function PageTitle({header, title, companyName = 'Finlay Stakes' }) {
 	return (
 		<Box>
-			<Typography variant="h5" color="#212844" sx={{fontSize: '20px', textTransform: 'capitalize', fontWeight: 'bold', fontFamily: 'Lato, sans-serif',}}>
-				{title || ''}
+			<Typography variant="h5" color="#212844" sx={{ textTransform: 'capitalize', fontWeight: 'bold', fontFamily: 'Inter, sans-serif',}}>
+				{header || ''}
 			</Typography>
 			<Breadcrumbs aria-label="breadcrumb"  separator="›" sx={{py: '6px'}}>
 				<Link href="/" color="#6560F0"  sx={{fontSize: '14px', textTransform: 'capitalize', fontWeight: 'light', textDecoration: 'none'}}>
